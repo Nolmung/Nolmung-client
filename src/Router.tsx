3d16c9aaf6b16325de92fcd38f0e28f760c0b595
hyorish03 <hyorish2001@gmail.com>
2024-11-25 12:31:36 +0900

    feat/SV-180: 장소 상세페이지 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,14 @@ import Main from '@pages/main';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Detail from './pages/detail';
+
 function Router() {
   return (
     <Suspense fallback={<div></div>}>
       <Routes>
         <Route path={ROUTE.MAIN()} element={<Main />} />
+        <Route path={ROUTE.DETAIL(':/placeId')} element={<Detail />} />
       </Routes>
     </Suspense>
   );
