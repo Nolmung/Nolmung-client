70230fc4fa8f26c8894d8510fd7f99db4aa50ad8
hyorish03 <hyorish2001@gmail.com>
2024-11-28 00:25:58 +0900

    fix/SV-114: 유틸함수 함수 표현식으로 변경 및 검색 결과 날짜 최신순으로 정렬

--- src/pages/search/utils/searchUtils.ts
+++ src/pages/search/utils/searchUtils.ts
@@ -1,38 +1,48 @@
 import { SearchHistoryItem } from '..';
 
-export function updateSearchHistory(
+export const updateSearchHistory = (
   searchHistory: SearchHistoryItem[],
   newSearchItem: SearchHistoryItem,
-): SearchHistoryItem[] {
-  const updatedHistory = [...searchHistory, newSearchItem];
-  localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
-  return updatedHistory;
-}
+): SearchHistoryItem[] => {
+  const updatedHistory = [newSearchItem, ...searchHistory];
 
-export function formatDate(date: Date): string {
+  const uniqueHistory = updatedHistory.reduce<SearchHistoryItem[]>(
+    (acc, curr) => {
+      if (!acc.find((item) => item.keyword === curr.keyword)) {
+        acc.push(curr);
+      }
+      return acc;
+    },
+    [],
+  );
+
+  localStorage.setItem('searchHistory', JSON.stringify(uniqueHistory));
+  return uniqueHistory;
+};
+export const formatDate = (date: Date): string => {
   return `${String(date.getMonth() + 1).padStart(2, '0')}.${String(
     date.getDate(),
   ).padStart(2, '0')}`;
-}
+};
 
-export function clearSearchHistory(): SearchHistoryItem[] {
+export const clearSearchHistory = (): SearchHistoryItem[] => {
   localStorage.removeItem('searchHistory');
   return [];
-}
+};
 
-export function deleteSearchKeyword(
+export const deleteSearchKeyword = (
   searchHistory: SearchHistoryItem[],
   id: number,
-): SearchHistoryItem[] {
+): SearchHistoryItem[] => {
   const updatedHistory = searchHistory.filter((item) => item.id !== id);
   localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
   return updatedHistory;
-}
+};
 
-export function handleInputChange(
+export const handleInputChange = (
   setState: React.Dispatch<React.SetStateAction<string>>,
-) {
+) => {
   return (e: React.ChangeEvent<HTMLInputElement>) => {
     setState(e.target.value);
   };
-}
+};
