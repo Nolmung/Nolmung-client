0c2c078297366f5c81b3557da3fb494db0f07bca
hyorish03 <hyorish2001@gmail.com>
2024-11-28 00:05:11 +0900

    feat/SV-114: 공통 버튼 컴포넌트 스토리 북 및 스타일링 수정

---
+++ src/common/components/button/Button.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { ButtonStyleProps } from './Button.type';
+
+export const S = {
+  Wrapper: styled.button<ButtonStyleProps>`
+    width: ${({ width }) => (width ? width : '142px')};
+    height: ${({ height }) => (height ? height : '48px')};
+    font-size: ${({ fontSize }) => (fontSize ? fontSize : '14px')};
+    font-weight: ${({ fontWeight }) => (fontWeight ? fontWeight : 600)};
+    background-color: ${({ backgroundColor }) =>
+      backgroundColor ? backgroundColor : '#F5F6F8'};
+    color: ${({ color }) => (color ? color : '#5D5D5D')};
+    text-align: center;
+    border-radius: ${({ borderRadius }) =>
+      borderRadius ? borderRadius : '12px'};
+  `,
+};
