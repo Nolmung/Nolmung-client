3e9d370339d214d5fb1bbb91567002356b168e8c
hyorish03 <hyorish2001@gmail.com>
2024-11-28 16:54:31 +0900

    fix/SV-135: 목록보기 버튼 삭제

--- src/pages/main/components/bottomSheet/index.tsx
+++ src/pages/main/components/bottomSheet/index.tsx
@@ -1,20 +1,15 @@
 import BottomSheetHandle from './Handle';
 import useBottomSheet from '../../hooks/useBottomSheet';
-import { FaList } from 'react-icons/fa';
 import { REF_HEIGHT } from '@/common/constants/ui';
 import { S } from '../../styles/BottomSheet.style';
 
 function BottomSheet() {
-  const { sheet, handleUp, content } = useBottomSheet({
+  const { sheet, content } = useBottomSheet({
     setIsBottomSheetOpen: () => {},
   });
 
   return (
     <S.Wrapper ref={sheet}>
-      <S.OpenButton onClick={handleUp}>
-        <FaList style={{ width: '16px', height: '16px', flexShrink: '0' }} />
-        <S.OpenButtonText> 목록 보기</S.OpenButtonText>
-      </S.OpenButton>
       <S.BottomSheetBody>
         <BottomSheetHandle />
         <div style={{ height: '100px', position: 'sticky' }}>메뉴</div>
