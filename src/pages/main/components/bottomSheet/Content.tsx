d928aeae6baedf17ae48cadd823130a0e049996d
hyorish03 <hyorish2001@gmail.com>
2024-11-28 17:59:26 +0900

    feat/SV-199: BottomSheet Content UI 구현

---
+++ src/pages/main/components/bottomSheet/Content.tsx
@@ -0,0 +1,52 @@
+import { FaStar } from 'react-icons/fa';
+import { S } from '../../styles/Content.style';
+import { IoHeartSharp } from 'react-icons/io5';
+import { IoHeartOutline } from 'react-icons/io5';
+import { useState } from 'react';
+import { MapPlace } from '@/service/apis/place/index.type';
+
+interface ContentProps {
+  place: MapPlace;
+}
+
+function Content({ place }: ContentProps) {
+  const [isLiked, setIsLiked] = useState<Boolean>(false);
+  const handleLikeClick = () => {
+    setIsLiked(!isLiked);
+  };
+  return (
+    <S.Wrapper>
+      <S.PlaceInfoWrapper>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+          <S.PlaceNameCategoryWrapper>
+            <S.PlaceName>{place.place_name}</S.PlaceName>
+            <S.PlaceCategory>{place.category}</S.PlaceCategory>
+          </S.PlaceNameCategoryWrapper>
+          <S.PlaceAddress>{place.road_address}</S.PlaceAddress>
+          <S.PlaceReviewWrapper>
+            <FaStar style={{ color: '#FFD700' }} />
+            {place.star_rating_avg}
+            <S.PlaceReviewCount>리뷰 {place.review_count}개</S.PlaceReviewCount>
+          </S.PlaceReviewWrapper>
+        </div>
+        <div>
+          {isLiked ? (
+            <S.IconWrapper onClick={handleLikeClick}>
+              <IoHeartSharp size={24} color="#FF4E3E" />
+            </S.IconWrapper>
+          ) : (
+            <S.IconWrapper onClick={handleLikeClick}>
+              <IoHeartOutline size={24} color="#FF4E3E" />
+            </S.IconWrapper>
+          )}
+        </div>
+      </S.PlaceInfoWrapper>
+      <S.PlaceImage
+        src="https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210721_132%2F1626855796685XVdK4_JPEG%2FW0db6n6s7HHCHrwhXAk9WPua.jpeg.jpg"
+        alt="시설 이미지"
+      />
+    </S.Wrapper>
+  );
+}
+
+export default Content;
--- src/pages/main/components/bottomSheet/index.tsx
+++ src/pages/main/components/bottomSheet/index.tsx
@@ -3,6 +3,8 @@ import useBottomSheet from '../../hooks/useBottomSheet';
 import { FaList } from 'react-icons/fa';
 import { REF_HEIGHT } from '@/common/constants/ui';
 import { S } from '../../styles/BottomSheet.style';
+import Content from './Content';
+import { placeMap } from '@/mocks/data/placeMap';
 
 function BottomSheet() {
   const { sheet, handleUp, content } = useBottomSheet({
@@ -17,22 +19,19 @@ function BottomSheet() {
       </S.OpenButton>
       <S.BottomSheetBody>
         <BottomSheetHandle />
-        <div style={{ height: '100px', position: 'sticky' }}>메뉴</div>
+        <div
+          style={{
+            height: '100px',
+            position: 'sticky',
+            backgroundColor: 'blue',
+          }}
+        >
+          메뉴
+        </div>
         <S.BottomSheetContentWrapper ref={content} refheight={REF_HEIGHT}>
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
-          <S.BottomSheetContentCard />
+          {placeMap.map((place) => (
+            <Content key={place.place_id} place={place} />
+          ))}
         </S.BottomSheetContentWrapper>
       </S.BottomSheetBody>
     </S.Wrapper>
