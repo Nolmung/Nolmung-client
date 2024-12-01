fbcaf960961fe7f0bb70af0604fdfa21a57dcced
hyorish03 <hyorish2001@gmail.com>
2024-12-01 15:36:08 +0900

    feat/SV-130: 사진 추가 버튼 기능 추가

--- src/pages/todayMungWrite/components/MediaGroup.tsx
+++ src/pages/todayMungWrite/components/MediaGroup.tsx
@@ -2,9 +2,31 @@ import { CameraIcon } from '@/assets/images/svgs';
 import S from '../styles/MediaGroup.style';
 
 function MediaGroup() {
+  const handleCliick = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.multiple = true;
+    input.click();
+
+    input.onchange = (e: Event) => {
+      const files = (e.target as HTMLInputElement).files;
+      if (files) {
+        const fileArray = Array.from(files);
+        fileArray.forEach((file) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = (e) => {
+            console.log((e.target as FileReader).result);
+          };
+        });
+      }
+    };
+  };
+
   return (
     <S.Wrapper>
-      <S.AddMediaButton>
+      <S.AddMediaButton onClick={handleCliick}>
         <CameraIcon width={24} height={24} />
       </S.AddMediaButton>
       <S.Media src="https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/28/111500268.2.jpg" />
