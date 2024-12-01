240a78dcbbe58679031073e269f17f3ac7ddb4af
hyorish03 <hyorish2001@gmail.com>
2024-12-02 00:53:57 +0900

    feat/SV-130: 동영상 추가 기능 구현

--- src/pages/todayMungWrite/components/MediaGroup.tsx
+++ src/pages/todayMungWrite/components/MediaGroup.tsx
@@ -19,7 +19,7 @@ function MediaGroup() {
   const handleAddMediaButtonCliick = async () => {
     const input = document.createElement('input');
     input.type = 'file';
-    input.accept = 'image/*';
+    input.accept = 'image/*, video/*';
     input.multiple = true;
     input.click();
 
