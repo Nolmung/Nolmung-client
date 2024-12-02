cf2804e304d3be42b347f6dcbaa4290b6b2e9928
youngsoon12 <enejwk789@gmail.com>
2024-12-03 00:20:43 +0900

    feat/SV-84: Header 뒤로가기 기능 추가

--- src/common/components/header/index.tsx
+++ src/common/components/header/index.tsx
@@ -2,20 +2,39 @@ import { GoBackIcon } from '@assets/images/svgs';
 import { S } from '@common/components/header/index.styles';
 import { HeaderType } from '@common/components/header/index.type';
 import { match } from 'ts-pattern';
+import { useNavigate } from 'react-router-dom';
 
 function Header({ title, showIcon, type }: HeaderType) {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
   return (
     <>
       {match(type)
         .with('TitleLeft', () => (
           <S.LeftHeaderArea>
-            {showIcon ? <GoBackIcon /> : <S.DummyIcon />}
+            {showIcon ? (
+              <GoBackIcon
+                onClick={handleGoBack}
+                style={{ cursor: 'pointer' }}
+              />
+            ) : (
+              <S.DummyIcon />
+            )}
             <S.LeftTitleArea>{title}</S.LeftTitleArea>
           </S.LeftHeaderArea>
         ))
         .with('TitleCenter', () => (
           <S.CenterHeaderArea>
-            {showIcon ? <GoBackIcon /> : <S.DummyIcon />}
+            {showIcon ? (
+              <GoBackIcon
+                onClick={handleGoBack}
+                style={{ cursor: 'pointer' }}
+              />
+            ) : (
+              <S.DummyIcon />
+            )}
             <S.CenterTitleArea>{title}</S.CenterTitleArea>
           </S.CenterHeaderArea>
         ))
