dbd0abefe70d44f3f614e0ddf37b1f7968c4ed52
ongheong <pudding4271@gmail.com>
2024-12-02 21:01:57 +0900

    feat/SV-208: Header 뒤로가기 버튼 클릭 시 '/'로 이동하게 변경

--- src/common/components/header/index.tsx
+++ src/common/components/header/index.tsx
@@ -1,21 +1,31 @@
 import { GoBackIcon } from '@assets/images/svgs';
 import { S } from '@common/components/header/index.styles';
 import { HeaderType } from '@common/components/header/index.type';
+import { useNavigate } from 'react-router-dom';
 import { match } from 'ts-pattern';
 
 function Header({ title, showIcon, type }: HeaderType) {
+  const navigate = useNavigate();
+  const handleBackButtonClick = () => {
+    navigate('/');
+  };
+
   return (
     <>
       {match(type)
         .with('TitleLeft', () => (
-          <S.LeftHeaderArea>
+          <S.LeftHeaderArea >
+            <S.IconWrapper onClick={handleBackButtonClick}>
             {showIcon ? <GoBackIcon /> : <S.DummyIcon />}
+            </S.IconWrapper>
             <S.LeftTitleArea>{title}</S.LeftTitleArea>
           </S.LeftHeaderArea>
         ))
         .with('TitleCenter', () => (
-          <S.CenterHeaderArea>
+          <S.CenterHeaderArea >
+            <S.IconWrapper onClick={handleBackButtonClick}>
             {showIcon ? <GoBackIcon /> : <S.DummyIcon />}
+            </S.IconWrapper>
             <S.CenterTitleArea>{title}</S.CenterTitleArea>
           </S.CenterHeaderArea>
         ))
