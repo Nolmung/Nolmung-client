345735da2fef6b739f2fad3925bad258945ddcdc
hyorish03 <hyorish2001@gmail.com>
2024-12-04 13:22:10 +0900

    feat/SV-153: 즐겨찾기 페이지에서 상세페이지로 이동 구현

--- src/pages/myFavorite/index.tsx
+++ src/pages/myFavorite/index.tsx
@@ -6,25 +6,36 @@ import { IoHeartSharp } from 'react-icons/io5';
 import { IoHeartOutline } from 'react-icons/io5';
 import { StarIcon } from '@/assets/images/svgs';
 import { PlaceCategoryMapping } from './constants/placeCategoryMapping';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from '@/common/constants/route';
 
 function MyFavorite() {
   const [value, setValue] = useState<PlaceCategory>('RESTAURANT');
   const [isLikedId, setIsLikedId] = useState<number>(0);
+
+  const navigate = useNavigate();
+
   const parsedAddress = (address: string) => {
     const parsed = address.split(' ').slice(0, 2).join(' ');
     return parsed;
-  }
+  };
   const handleCategoryClick = (value: PlaceCategory) => {
     setValue(value);
   };
 
-  {/** @Todo API 부착 시, 해당 장소의 즐겨찾기 여부를 토글하도록 변경 */}
+  {
+    /** @Todo API 부착 시, 해당 장소의 즐겨찾기 여부를 토글하도록 변경 */
+  }
   const handleLikeClick = (id: number) => {
     if (isLikedId === id) {
       setIsLikedId(0);
     } else {
       setIsLikedId(id);
     }
+  };
+
+  const navigateToDetail = (placeId: number) => {
+    navigate(ROUTE.DETAIL(placeId));
   };
 
   return (
@@ -42,7 +53,10 @@ function MyFavorite() {
       </S.CategoryWrapper>
       <S.PlaceWrapper>
         {placeMap.map((place) => (
-          <S.PlaceCard key={place.place_id}>
+          <S.PlaceCard
+            onClick={() => navigateToDetail(place.place_id)}
+            key={place.place_id}
+          >
             <S.ImageWrapper>
               <S.PlaceImage src={place.place_img_url} alt={place.place_name} />
               {place.place_id === isLikedId ? (
@@ -56,10 +70,12 @@ function MyFavorite() {
               )}
             </S.ImageWrapper>
             <S.PlaceInfo>
-              <S.PlaceLocation>{parsedAddress(place.road_address)}</S.PlaceLocation>
+              <S.PlaceLocation>
+                {parsedAddress(place.road_address)}
+              </S.PlaceLocation>
               <S.PlaceName>{place.place_name}</S.PlaceName>
               <S.ReviewWrapper>
-                <StarIcon width={14} height={14}/>
+                <StarIcon width={14} height={14} />
                 <S.StarRating>{place.star_rating_avg}</S.StarRating>
                 <S.ReviewCount>리뷰 {place.review_count}</S.ReviewCount>
               </S.ReviewWrapper>
