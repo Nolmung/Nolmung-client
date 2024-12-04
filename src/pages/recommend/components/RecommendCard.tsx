38413c6720cfbfa1ded7e17051cc2ea144109926
hyorish03 <hyorish2001@gmail.com>
2024-12-04 09:37:24 +0900

    feta/SV-98: 상세페이지로 라우팅 구현

--- src/pages/recommend/components/RecommendCard.tsx
+++ src/pages/recommend/components/RecommendCard.tsx
@@ -1,5 +1,7 @@
 import { MapPlace } from '@/service/apis/place/index.type';
 import S from '../styles/RecommendCard.style';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from '@/common/constants/route';
 
 interface RecommendCardProps {
   title: string;
@@ -8,13 +10,19 @@ interface RecommendCardProps {
 }
 
 function RecommendCard({ data, title, explanation }: RecommendCardProps) {
+  const navigate = useNavigate();
+
+  const navigateToDetail = (placeId: number) => {
+    navigate(ROUTE.DETAIL(placeId));
+  };
+
   return (
     <S.Wrapper>
       <S.Title>{title}</S.Title>
       <S.TitleExplanation>{explanation}</S.TitleExplanation>
       <S.PlaceList>
         {data.map((mock) => (
-          <S.PlaceWrapper>
+          <S.PlaceWrapper onClick={() => navigateToDetail(mock.place_id)}>
             <S.PlaceImage src={mock.place_img_url} alt="장소 이미지" />
             <S.NameAddressWrapper>
               <S.PlaceName>{mock.place_name}</S.PlaceName>
