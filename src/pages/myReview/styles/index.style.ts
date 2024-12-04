cd13572183b9d255db4b5c2b663296ae8fee5e8b
hyorish03 <hyorish2001@gmail.com>
2024-12-04 11:14:30 +0900

    fix/SV-155: 내 리뷰 페이지 height 수정

--- src/pages/myReview/styles/index.style.ts
+++ src/pages/myReview/styles/index.style.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const S = {
   Wrapper: styled.div`
     width: 100%;
-    height: calc(100% - ${HEADER_HEIGHT}px);
+    height: calc(100vh - ${HEADER_HEIGHT}px);
     overflow-y: scroll;
   `,
 };
