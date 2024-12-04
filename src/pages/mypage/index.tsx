e2d820dfcf7aa38c21e69b94d8f3f48eba1d1c6e
ongheong <pudding4271@gmail.com>
2024-12-04 10:01:04 +0900

    feat/SV-162: 마이페이지 UI 구현

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,21 +1,66 @@
 import S from './index.styles';
+import { userData } from '@/mocks/data/userData';
+import { dogData } from '@/mocks/data/dogData';
+import kakaoIcon from '@/assets/images/pngs/kakao_icon_image.png';
+import PetProfile from './components/profile';
+import { HeartIcon, LogoutIcon, NoticeIcon, ReviewListIcon, UserEditIcon } from '@/assets/images/svgs';
+import { useState } from 'react';
 
 function Mypage() {
+  const [editId, setEditId] = useState<number>(0);
+
   return (
     <S.Wrapper>
       <S.ProfileWrapper>
         <S.MyProfileCard>
-          {/** @Todo userInfo */}
+          <S.ProfileImg
+            width={50}
+            height={50}
+            src={userData.userProfileImage}
+          />
+          <S.ProfileTextWrapper>
+            <S.NameWrapper>
+              <S.ProfileName>{userData.userNickname}</S.ProfileName>
+              <UserEditIcon width={15} height={15} />
+            </S.NameWrapper>
+            <S.ProfileEmailWrapper>
+              <S.KaKaoIconImg src={kakaoIcon} />
+              <S.ProfileEmail>{userData.userEmail}</S.ProfileEmail>
+            </S.ProfileEmailWrapper>
+          </S.ProfileTextWrapper>
         </S.MyProfileCard>
         <S.PetProfileWrapper>
-          {/** @Todo petInfo map 돌기, 펫 카드 프로필 따로 분리하여 관리하기 */}
-        <S.PetProfileCard></S.PetProfileCard>
+          {dogData.length &&
+            dogData.map((data) => (
+              <PetProfile
+                isActive={data.dogId === editId}
+                data={data}
+                editId={editId}
+                setEditId={setEditId}
+              />
+            ))}
         </S.PetProfileWrapper>
       </S.ProfileWrapper>
-      {/** @Todo 목록 버튼 컴포넌트 구현 */}
-      {/** @Todo 로그아웃 버튼 컴포넌트 구현 */}
+      <S.ListWrapper>
+        <S.ListContainer>
+          <HeartIcon width={19} height={19} />
+          즐겨찾기 목록
+        </S.ListContainer>
+        <S.ListContainer>
+          <NoticeIcon width={22} height={22} />
+          공지사항
+        </S.ListContainer>
+        <S.ListContainer>
+          <ReviewListIcon width={19} height={19} />
+          내 리뷰 모아보기
+        </S.ListContainer>
+        <S.ListContainer>
+          <LogoutIcon width={19} height={19} />
+          로그아웃
+        </S.ListContainer>
+      </S.ListWrapper>
     </S.Wrapper>
-  )
+  );
 }
 
 export default Mypage;
