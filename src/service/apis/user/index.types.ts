aa72c59e16a1d35ca192643744cfe08d1c8006f8
hyorish03 <hyorish2001@gmail.com>
2024-12-05 21:09:33 +0900

    fix/SV-132: 시설 상세페이지 dog acceptSize 타입 All 추가

--- src/service/apis/user/index.types.ts
+++ src/service/apis/user/index.types.ts
@@ -1,19 +1,4 @@
 /** user 도메인 관련 API Response 타입 */
-
-export interface DogType {
-  dogId: number;
-  dogName: string;
-  dogType: string;
-  birth: string;
-  profileUrl: string;
-  gender: 'MALE' | 'FEMALE';
-  size: DogSize;
-  neuterYn: boolean;
-}
-export interface DogsResponse {
-  data: DogType[];
-}
-
 export interface ReviewLabel {
   id: number;
   label: string;
@@ -40,7 +25,7 @@ export interface UserType {
   userEmail: string;
 }
 
-export type DogSize = 'S' | 'M' | 'L';
+export type DogSize = 'S' | 'M' | 'L' | 'ALL';
 
 export type Age = 10 | 20 | 30 | 40 | 50;
 
