3a592f85a7374af7090780ae323a034eaf8088b9
youngsoon12 <enejwk789@gmail.com>
2024-12-06 16:47:29 +0900

    fix/SV-85: 불필요한 타입 제거

--- src/pages/todaymung/queries/index.ts
+++ src/pages/todaymung/queries/index.ts
@@ -1,9 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { TodaymungDataType } from '../types/TodayMungData.type';
 import { getTodaymungList } from '@/service/apis/diary';
 
 export const useTodaymungData = () => {
-  return useQuery<TodaymungDataType>({
+  return useQuery({
     queryKey: ['todaymungData'],
     queryFn: getTodaymungList,
     staleTime: 1000 * 60 * 5,
