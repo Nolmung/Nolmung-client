79ec1d9fa1db694dc36165e0a90eabd9362c3224
hyorish03 <hyorish2001@gmail.com>
2024-12-08 23:39:21 +0900

    chore/SV-250: 사용하지 않는 import 주석처리

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,7 +1,7 @@
 import { S } from './styles/signUp.styles';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import convertAddressToLatlng from './utils/convertAddressToLatlng';
+// import convertAddressToLatlng from './utils/convertAddressToLatlng';
 
 const locations = [
   '서울특별시',
