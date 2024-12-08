b7ae8c843cfa90d70946ec43bdb93418de42257e
hyorish03 <hyorish2001@gmail.com>
2024-12-08 22:56:17 +0900

    feat/SV-99: 게스트 유저에 따른 추천 페이지 분기처리

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,95 +1,10 @@
-import RecommendCard from './components/RecommendCard';
-import S from './styles/index.style';
-import useSetDocumentTitle from '@/common/hooks/useSetDocumentTitle';
-import {
-  useRecommendBookmarks,
-  useRecommendNearby,
-  useRecommendSimilarBookmarks,
-  useRecommendWeight,
-} from './queries';
-import { useGetUser } from '../my/hooks';
-import { useGetDogs } from '../todaymungWrite/queries';
+import GuestRecommend from "./index.guest";
+import UserRecommend from "./index.user";
 
 function Recommend() {
-  useSetDocumentTitle('추천');
-  const {
-    data: weightData,
-    isLoading: weightLoading,
-    isError: weightError,
-  } = useRecommendWeight();
-  const {
-    data: similarBookmarks,
-    isLoading: similarLoading,
-    isError: similarError,
-  } = useRecommendSimilarBookmarks();
-  const {
-    data: nearByData,
-    isLoading: nearbyLoading,
-    isError: nearbyError,
-  } = useRecommendNearby();
-  const {
-    data: bookmarks,
-    isLoading: bookmarksLoading,
-    isError: bookmarkError,
-  } = useRecommendBookmarks();
-  const {
-    data: userInfo,
-    isLoading: userLoading,
-    isError: userError,
-  } = useGetUser();
+    const isGuest = localStorage.getItem('accessToken') === null || localStorage.getItem('accessToken') === undefined;
 
-  if (
-    userLoading ||
-    bookmarksLoading ||
-    weightLoading ||
-    similarLoading ||
-    nearbyLoading
-  ) {
-    return <div>Loading...</div>;
-  }
-
-  if (
-    userError ||
-    bookmarkError ||
-    weightError ||
-    similarError ||
-    nearbyError
-  ) {
-    return <div>Error...</div>;
-  }
-
-  return (
-    <S.Wrapper>
-      {similarBookmarks.length > 0 && (
-        <RecommendCard
-          title={`${userInfo?.userNickname} 님이 좋아할 만한 공간`}
-          explanation="개발자가 피땀흘려 고른 공간"
-          data={similarBookmarks}
-        />
-      )}
-      {nearByData.length > 0 && (
-        <RecommendCard
-          title={`${userInfo?.userNickname} 님 근처 핫 플레이스`}
-          explanation="옆집 강아지들은 이미 다 가본데라고?"
-          data={nearByData}
-        />
-      )}
-      {bookmarks.length > 0 && (
-        <RecommendCard
-          title="다른 집사들이 좋아요를 많이 누른 공간"
-          explanation="아니 글쎄 ~ 여기가 그렇게 핫플이래~"
-          data={bookmarks}
-        />
-      )}
-      {weightData.length > 0 && (
-        <RecommendCard
-          title="우리 아이 친구들이 다 가본 곳"
-          explanation="우리 아이들이 갈 수 있는 곳을 추천해드려요"
-          data={weightData}
-        />
-      )}
-    </S.Wrapper>
-  );
+    return isGuest ? <GuestRecommend/> : <UserRecommend/>;
+  
 }
-
 export default Recommend;
