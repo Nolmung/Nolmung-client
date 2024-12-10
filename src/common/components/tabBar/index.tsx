c0b1bc576cd81c7514c493af97cfb11fe17c1b5a
hyorish03 <hyorish2001@gmail.com>
2024-12-10 19:41:01 +0900

    feat/SV-215: 비로그인 탭바 이동시 로그인 유도 모달 구현 및 오늘멍 작성 중 페이지 이탈시 store 초기화

--- src/common/components/tabBar/index.tsx
+++ src/common/components/tabBar/index.tsx
@@ -1,15 +1,51 @@
 import { S } from '@common/components/tabBar/index.styles';
 import { TabBarType } from '@common/components/tabBar/index.type';
 import tabItems from '@common/constants/tabBarItems';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { isTabActive } from './utils/tabUtils';
+import { useLoginPromptModalStore } from '@/stores/useLoginPromptModalStore';
+import useIsReviewEmpty from '@/common/utils/useIsReviewEmpty';
+import useIsTodaymungEmpty from '@/common/utils/useIsTodaymungEmpty';
+import { useReviewStore } from '@/pages/todaymungPlaceRegist/stores/reviewStore';
+import { useTodayMungStore } from '@/pages/todaymungWrite/stores/todayMungStore';
 
 function TabBar() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { open } = useLoginPromptModalStore();
+  const isReviewEmpty = useIsReviewEmpty();
+  const isTodaymungEmpty = useIsTodaymungEmpty();
+
+  const { deleteReviewAll } = useReviewStore();
+  const { deleteTodaymungAll } = useTodayMungStore();
+
+  const isLoggedIn = localStorage.getItem('accessToken');
+
+  const handleTabIconClick = (path: string) => {
+    if (!isLoggedIn) {
+      if (path === '/todaymung' || path === '/my') {
+        open();
+        return;
+      }
+    } else {
+      if (location.pathname === '/todaymung/write') {
+        if (!isReviewEmpty || !isTodaymungEmpty) {
+          window.confirm('작성중인 글이 있습니다. 이동하시겠습니까?');
+          deleteReviewAll();
+          deleteTodaymungAll();
+        }
+      }
+      navigate(path);
+    }
+  };
+
   return (
     <S.Wrapper>
       {tabItems.map((item: TabBarType) => (
-        <S.IconArea to={item.path} key={item.path}>
+        <S.IconArea
+          onClick={() => handleTabIconClick(item.path)}
+          key={item.path}
+        >
           {isTabActive(item.path, location.pathname) ? (
             <item.activeIcon />
           ) : (
