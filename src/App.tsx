75786b52ca6f97782fd161fa133ef627735fafbe
hyorish03 <hyorish2001@gmail.com>
2024-12-10 19:42:09 +0900

    feat/SV-215: vh 초기화 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const queryClient = new QueryClient();
-  const setScreenHeight = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-
-  useEffect(() => {
-    setScreenHeight();
-    window.addEventListener('resize', setScreenHeight);
-    return () => {
-      window.removeEventListener('resize', setScreenHeight);
-    };
-  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
