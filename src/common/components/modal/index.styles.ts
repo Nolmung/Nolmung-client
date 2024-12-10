0bf735478afcccad9dfaf70fe456c5ff2d9ec1b8
hyorish03 <hyorish2001@gmail.com>
2024-12-10 21:02:01 +0900

    fix/SV-215: 모달 max-width 수정

--- src/common/components/modal/index.styles.ts
+++ src/common/components/modal/index.styles.ts
@@ -4,13 +4,13 @@ export const S = {
   ModalWrapper: styled.div`
     position: fixed;
     top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     z-index: 1500;
+    max-width: 423px;
   `,
 
   ModalBackground: styled.div`
