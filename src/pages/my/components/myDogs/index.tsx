ee1ce67da5c562664ee2dc3b098cb13c669b7eea
youngsoon12 <enejwk789@gmail.com>
2024-12-12 14:14:16 +0900

    fix/SV-325: 강아지 등록페이지 잘리는 거 높이 수정

--- src/pages/my/components/myDogs/index.tsx
+++ src/pages/my/components/myDogs/index.tsx
@@ -3,20 +3,30 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import PetProfileCard from '../profile/index';
 import { ROUTE } from '@common/constants/route';
 import { PlusIcon } from '@/assets/images/svgs';
+import { useGetDogs } from '@/pages/todaymungWrite/queries';
 
 const MyDogs = () => {
-  
+  const a = useGetDogs();
+  console.log(a);
   const { state } = useLocation();
   const { dogData, nickname } = state || {};
   const navigate = useNavigate();
   const handleDogRegisterClick = () => {
-    navigate(ROUTE.MY_DOGS_ADD(), { state: { dogData: dogData, nickname: nickname!} });
+    navigate(ROUTE.MY_DOGS_ADD(), {
+      state: { dogData: dogData, nickname: nickname! },
+    });
   };
   return (
     <S.Wrapper>
       {dogData && dogData.length > 0 ? (
         dogData.map((data: any) => {
-          return <PetProfileCard data={data} key={data.dogId} userNickname={nickname}/>;
+          return (
+            <PetProfileCard
+              data={data}
+              key={data.dogId}
+              userNickname={nickname}
+            />
+          );
         })
       ) : (
         <div>존재하지 않아용</div>
