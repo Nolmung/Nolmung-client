8f76bc342ebe4bcee2434eca44a22a507fb4307e
hyorish03 <hyorish2001@gmail.com>
2024-12-15 14:16:06 +0900

    feat/SV-368: DogCard 및 MediaGroup 컴포넌트에 GA 이벤트 부착

--- src/pages/todaymungWrite/components/DogCard.tsx
+++ src/pages/todaymungWrite/components/DogCard.tsx
@@ -1,7 +1,7 @@
 import S from '../styles/DogCard.style';
 import { useTodayMungStore } from '../stores/todayMungStore';
 import { DogType } from '@/service/apis/dog/index.type';
-
+import ReactGA from 'react-ga4';
 interface DogCardProps {
   data: DogType;
 }
@@ -12,8 +12,18 @@ function DogCard({ data }: DogCardProps) {
   const handlePetButtonClick = () => {
     if (isDogSelected) {
       deleteDogs(data.dogId);
+      ReactGA.event({
+        category: 'User Interaction',
+        action: 'Click Delete Dog Card',
+        label: `User clicked on dog: ${data.dogName}`,
+      });
     } else {
       addDogs(data.dogId);
+      ReactGA.event({
+        category: 'User Interaction',
+        action: 'Click Add Dog Card',
+        label: `User clicked on dog: ${data.dogName}`,
+      });
     }
   };
   return (
