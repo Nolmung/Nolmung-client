7ba7a2cd5cd9aa9ba8a53769818ce93cca91d24f
hyorish03 <hyorish2001@gmail.com>
2024-12-15 13:39:56 +0900

    feat/SV-368: login 페이지 이벤트 부착

--- src/pages/login/components/KakaoLoginButton.tsx
+++ src/pages/login/components/KakaoLoginButton.tsx
@@ -1,8 +1,14 @@
 import { S } from '../styles/login.styles';
+import ReactGA from 'react-ga4';
 
 function KakaoLoginButton() {
   const handleKakaoLogin = () => {
     window.location.href = import.meta.env.VITE_KAKAO_API_URL;
+    ReactGA.event({
+      category: 'User',
+      action: 'Click Kakao Login',
+      label: 'User clicked on Kakao login button',
+    });
   };
 
   return (
