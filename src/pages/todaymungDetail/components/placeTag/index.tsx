2bb68f5a67177ca19b3b8da48579da9a45e55c84
hyorish03 <hyorish2001@gmail.com>
2024-12-16 19:57:26 +0900

    fix/SV-368: 장소 클릭시 해당 장소 상세로 이동

--- src/pages/todaymungDetail/components/placeTag/index.tsx
+++ src/pages/todaymungDetail/components/placeTag/index.tsx
@@ -1,14 +1,20 @@
 import { S } from '../../styles/PlaceTagCard.style';
 import { LocationPinIcon } from '@assets/images/svgs';
 import { PlacesType } from '../../types/DiaryType';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE } from '@/common/constants/route';
 
 interface PlaceCardProps {
   data: PlacesType;
 }
 
 const PlaceTagCard = ({ data }: PlaceCardProps) => {
+  const navigate = useNavigate();
+  const handleLocationClick = () => {
+    navigate(ROUTE.DETAIL(data.placeId));
+  };
   return (
-    <S.Wrapper>
+    <S.Wrapper onClick={handleLocationClick}>
       <LocationPinIcon width={20} height={20} />
       <S.LocationName>{data.placeName}</S.LocationName>
     </S.Wrapper>
