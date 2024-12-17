edf79fa38e9638f9812b812f10ae6a6622dfdfe1
hyorish03 <hyorish2001@gmail.com>
2024-12-17 10:51:11 +0900

    feat/SV-368: 오늘멍 상세보기 뒤로가기 수정

--- src/common/components/layout/index.tsx
+++ src/common/components/layout/index.tsx
@@ -124,7 +124,7 @@ function Layout({ children }: LayoutProps) {
         type: 'TitleCenter',
       });
       setHandleBackButtonClick(() => () => {
-        navigate('/todaymung');
+        navigate(-1);
         ReactGA.event({
           category: 'goBack',
           action: 'click goBack button',
