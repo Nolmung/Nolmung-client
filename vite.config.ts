f41744ec4351bc7094572a9180d0e2380468ed27
hyorish03 <hyorish2001@gmail.com>
2024-12-18 16:26:06 +0900

    feat/SV-377: 스크롤 이벤트 debounce 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import { createHtmlPlugin } from 'vite-plugin-html';
-
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -10,6 +9,10 @@ export default defineConfig(({ mode }) => {
   return {
     define: {
       global: 'globalThis', // global을 globalThis로 매핑
+    },
+    build: {
+      // minify: 'esbuild', // 기본값, esbuild를 사용하여 최소화
+      minify: 'terser',
     },
     plugins: [
       react(),
